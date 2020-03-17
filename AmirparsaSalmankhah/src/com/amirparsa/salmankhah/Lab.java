7948ceedea1de6c830cd2db90c0253b09be75c31
Amirparsa <Amirparsa.s@aut.ac.ir>
2020-03-17 19:05:29 +0330

    Add Lab class. Edit Run class

---
+++ AmirparsaSalmankhah/src/com/amirparsa/salmankhah/Lab.java
@@ -0,0 +1,127 @@
+package com.amirparsa.salmankhah;
+
+/**
+ * Represents a lab
+ *
+ * @author Amirparsa Salmankhah
+ * @version 1.0
+ */
+public class Lab {
+    //Students of the lab
+    private Student[] students;
+    //Average score of students
+    private int avg;
+    //Day of the lab
+    private String day;
+    //Capacity of the lab
+    private int capacity;
+    //Current size of the lab
+    private int currentSize;
+
+    /**
+     * Main constructor with 2 parameters
+     *
+     * @param capacity Capacity of the lab
+     * @param day      Day of the lab
+     */
+    public Lab(int capacity, String day) {
+        currentSize = 0;
+        avg = 0;
+        this.capacity = capacity;
+        this.day = day;
+        students = new Student[capacity];
+    }
+
+    /**
+     * Add students to the lab
+     *
+     * @param std student
+     */
+    public void enrollStudent(Student std) {
+        if (currentSize < capacity) {
+            students[currentSize] = std;
+            currentSize++;
+        } else
+            System.out.println("Lab is full!!!");
+    }
+
+    /**
+     * Printing details of students and the average
+     */
+    public void print() {
+        for (int i = 0; i < currentSize; i++)
+            students[i].print();
+        System.out.println("The average is: " + avg);
+    }
+
+    /**
+     * Students getter
+     *
+     * @return an array of Students
+     */
+    public Student[] getStudents() {
+        return students;
+    }
+
+    /**
+     * Students setter
+     *
+     * @param students an array of Students
+     */
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    /**
+     * Average getter
+     *
+     * @return an int - Average
+     */
+    public int getAvg() {
+        return avg;
+    }
+
+    /**
+     * Calculate the average
+     */
+    public void calculateAvg() {
+        int sum = 0;
+        for (int i = 0; i < currentSize; i++)
+            sum += students[i].getGrade();
+        avg = sum / currentSize;
+    }
+
+    /**
+     * Day getter
+     *
+     * @return a String - Day of the lab
+     */
+    public String getDay() {
+        return day;
+    }
+
+    /**
+     * Day setter
+     *
+     * @param day Day of the lab
+     */
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    /**
+     * Capacity setter
+     * @param capacity Capacity of the lab
+     */
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    /**
+     * Capacity getter
+     * @return an integer - Capacity of the lab
+     */
+    public int getCapacity() {
+        return capacity;
+    }
+}
--- AmirparsaSalmankhah/src/com/amirparsa/salmankhah/Run.java
+++ AmirparsaSalmankhah/src/com/amirparsa/salmankhah/Run.java
@@ -2,21 +2,19 @@ package com.amirparsa.salmankhah;
 
 public class Run {
     public static void main(String[] args) {
+        Lab lab = new Lab(15,"Saturday");
         Student std1 = new Student("Ehsan","Edalat","9031066");
         Student std2 = new Student("Seyed","Ahmadpanah","9031806");
         Student std3 = new Student("Ahmad","Asadi","9031054");
 
-        std1.print();
         std1.setGrade(15);
-        std1.print();
-
-        std2.print();
         std2.setGrade(11);
-        std2.print();
+        std3.setGrade(17);
 
-        std3.print();
-        std3.setFirstName("Hamidreza");
-        std3.print();
-
+        lab.enrollStudent(std1);
+        lab.enrollStudent(std2);
+        lab.enrollStudent(std3);
+        lab.calculateAvg();
+        lab.print();
     }
 }
