fec52a42e649132c01ba80a2c079695629868675
Amirparsa <Amirparsa.s@aut.ac.ir>
2020-03-21 21:09:13 +0430

    Add Music class

---
+++ AmirparsaSalmankhah/music-organizer/Music.java
@@ -0,0 +1,94 @@
+package com.amirparsa.salmankhah;
+
+import java.security.PublicKey;
+
+/**
+ * Represents a music with its name, singer and production year;
+ *
+ * @author Amirparsa Salmnakhah
+ * @version 1.0
+ */
+public class Music {
+    //Music name
+    private String name;
+    //Music singer
+    private String singer;
+    //Music production year
+    private int productYear;
+
+    /**
+     * Constructor with no parameters
+     */
+    public Music() {
+        name = new String("");
+        singer = new String("");
+        productYear = 0;
+    }
+
+    /**
+     * Main constructor with 3 parameters;
+     *
+     * @param name        Music name
+     * @param singer      Music singer
+     * @param productYear Music production year
+     */
+    public Music(String name, String singer, int productYear) {
+        this.name = name;
+        this.singer = singer;
+        this.productYear = productYear;
+    }
+
+    /**
+     * Name getter
+     *
+     * @return Name of the music
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Name setter
+     *
+     * @param name Name of the music
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Singer getter
+     *
+     * @return Singer of the music
+     */
+    public String getSinger() {
+        return singer;
+    }
+
+    /**
+     * Singer setter
+     *
+     * @param singer Singer of the music
+     */
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    /**
+     * Production year getter
+     *
+     * @return Production year of the music
+     */
+    public int getProductYear() {
+        return productYear;
+    }
+
+    /**
+     * Production year setter
+     *
+     * @param productYear Production year of the music
+     */
+    public void setProductYear(int productYear) {
+        this.productYear = productYear;
+    }
+}
