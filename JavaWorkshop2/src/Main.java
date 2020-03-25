2ce961cf7c47677481d92372a395724fbca0f967
alifadaei <59139628+alifadaei@users.noreply.github.com>
2020-03-26 00:42:31 +0430

    Add files via upload

---
+++ JavaWorkshop2/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * at first we want to test the students class functionality
+         */
+//        Student std1 = new Student("Ehsan", "Edalat", "9031066");
+//        Student std2 = new Student("Seyed", "Ahmadpanah", "9031806");
+//        Student std3 = new Student("Ahmad", "Asadi", "9031054");
+//        std1.print();
+//        std1.setGrade(15);
+//        std1.print();
+//        std2.print();
+//        std2.setGrade(11);
+//        std2.print();
+//        std3.print();
+//        std3.setFirstName("HamidReza");
+//        std3.print();
+
+        /**
+         *  now we want to test the lab class functionality
+         */
+
+//        Lab lab = new Lab(3, "1 Farvardin");
+//        lab.enrollStudent(new Student("ali", "fadaei", "9831130", 20));
+//        lab.enrollStudent(new Student("hossein", "jafari", "9831131", 10));
+//        lab.print();
+
+        /**
+         * now we want to know is passing an object to a method, called by value or reference? look at below:
+          */
+
+//        Student std = new Student("ali", "fadaei", "1234567", 100);
+//        Student[] students = new Student[3];
+//        students[0] = std;
+//        students[0].print(); // prints 100
+//        std.setGrade(50); // change std grades
+//        students[0].print(); // prints 50! wow! the reference is changed! >> object call by reference!
+//
+
+
+
+
+    }
+}
