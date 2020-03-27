dca436cbf02de3f005bed682cbdd6a24d922937a
SaeedShafie <saeedshafie@aut.ac.ir>
2020-03-27 21:45:47 +0430

    add MusicPlayer

---
+++ SaeedShafie/MusicPlayer/src/MusicCollection.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+/**
+ * A class to hold details of audio files.
+ *
+ * @author David J. Barnes and Michael K�lling
+ * @version 2011.07.31
+ */
+public class MusicCollection
+{
+    // An ArrayList for storing the file names of music files.
+    private ArrayList<String> files;
+    // A player for the music files.
+    private MusicPlayer player;
+
+    /**
+     * Create a MusicCollection
+     */
+    public MusicCollection()
+    {
+        files = new ArrayList<String>();
+        player = new MusicPlayer();
+
+    }
+
+    /**
+     * Add a file to the collection.
+     * @param filename The file to be added.
+     */
+    public void addFile(String filename)
+    {
+        files.add(filename);
+
+    }
+
+    /**
+     * Return the number of files in the collection.
+     * @return The number of files in the collection.
+     */
+    public int getNumberOfFiles()
+    {
+        return files.size();
+    }
+
+    /**
+     * List a file from the collection.
+     * @param index The index of the file to be listed.
+     */
+    public void listFile(int index)
+    {
+        if(validIndex(index)) {
+            System.out.println(files.get(index));
+        }
+    }
+
+    /**
+     * Show a list of all the files in the collection.
+     */
+    public void listAllFiles()
+    {
+        int n = 0;
+        for(String song : files) {
+            System.out.printf("%d",n);
+            System.out.println(song);
+            n++;
+        }
+
+    }
+
+    /**
+     * Remove a file from the collection.
+     * @param index The index of the file to be removed.
+     */
+    public void removeFile(int index)
+    {
+        if(validIndex(index)) {
+            files.remove(index);
+        }
+
+    }
+
+    /**
+     * Start playing a file in the collection.
+     * Use stopPlaying() to stop it playing.
+     * @param index The index of the file to be played.
+     */
+    public void startPlaying(int index)
+    {
+        player.playing(files.get(index));
+    }
+
+    /**
+     * Stop the player.
+     */
+    public void stopPlaying()
+    {
+        player.stop();
+    }
+
+
+    /**
+     * Determine whether the given index is valid for the collection.
+     * Print an error message if it is not.
+     * @param index The index to be checked.
+     * @return true if the index is valid, false otherwise.
+     */
+    private boolean validIndex(int index)
+    {
+        if(files.size()>=index && index>=0){
+            return true;
+        }
+        return false;
+    }
+
+
+    }
+}
